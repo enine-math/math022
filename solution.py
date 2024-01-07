31ec30b0df46cb6d1d4431cfde5041be750bec7e
Vishal Apte <vishalapte@gmail.com>
2024-01-07 17:38:57 +0530

    Name Scores

---
+++ solution.py
@@ -0,0 +1,14 @@
+"""
+Name Scores
+"""
+
+
+def answer():
+    return -1
+
+
+def solver(names: str):
+    """
+    names is the name of the file with all the names for processing
+    """
+    return -1
